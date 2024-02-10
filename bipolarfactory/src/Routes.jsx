7e59fd6cd5616d1e90c5825b7667cdc2495ccacc
Abhishek kumar <abhi.arya97161@gmail.com>
2024-02-10 12:02:11 +0530

    assets, fonts and images added

---
+++ bipolarfactory/src/Routes.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+const Tables = React.lazy(() => import("pages/Tables"));
+const Forms = React.lazy(() => import("pages/Forms"));
+const Cards = React.lazy(() => import("pages/Cards"));
+const Charts = React.lazy(() => import("pages/Charts"));
+const Pages = React.lazy(() => import("pages/Pages"));
+const ProjectRoutes = () => {
+    return (
+        <React.Suspense fallback={<>Loading...</>}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="*" element={<NotFound />} />
+                    <Route path="/pages" element={<Pages />} />
+                    <Route path="/charts" element={<Charts />} />
+                    <Route path="/cards" element={<Cards />} />
+                    <Route path="/forms" element={<Forms />} />
+                    <Route path="/tables" element={<Tables />} />
+                </Routes>
+            </Router>
+        </React.Suspense>
+    );
+};
+export default ProjectRoutes;
